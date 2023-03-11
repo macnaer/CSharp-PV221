1b435971a53f617e3c1acf9f950b73d30630e9fb
master <master@A33PC15>
2023-03-11 20:03:42 +0200

    Add bench

---
+++ 03. List/Program.cs
@@ -0,0 +1,86 @@
+﻿using Benchmark;
+using BenchmarkDotNet.Running;
+
+namespace _03._List
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //List<string> countries = new List<string>();
+
+            //countries.Add("Ukraine");
+            //countries.Add("France");
+            //countries.Add("Swizerland");
+
+            //Console.WriteLine("=====================Add====================");
+            //Console.WriteLine($"List count: {countries.Count}");
+            //foreach (string country in countries)
+            //{
+            //    Console.WriteLine(country);
+            //}
+
+            //Console.WriteLine("=====================Sort====================");
+            //countries.Sort();
+            //Console.WriteLine($"List count: {countries.Count}");
+            //foreach (string country in countries)
+            //{
+            //    Console.WriteLine(country);
+            //}
+
+            //Console.WriteLine("=====================Revers====================");
+            //countries.Reverse();
+            //Console.WriteLine($"List count: {countries.Count}");
+            //foreach (string country in countries)
+            //{
+            //    Console.WriteLine(country);
+            //}
+
+            //Console.WriteLine("=====================Insert====================");
+            //countries.Insert(1, "Germany");
+            //Console.WriteLine($"List count: {countries.Count}");
+            //foreach (string country in countries)
+            //{
+            //    Console.WriteLine(country);
+            //}
+
+            //Console.WriteLine("=====================AddRange====================");
+            //string[] strings = { "Italy", "Portugal", "Poland" };
+            //countries.AddRange(strings);
+            //Console.WriteLine($"List count: {countries.Count}");
+            //foreach (string country in countries)
+            //{
+            //    Console.WriteLine(country);
+            //}
+
+            //Console.WriteLine("=====================RemoveByName====================");
+            //countries.Remove("Germany");
+            //Console.WriteLine($"List count: {countries.Count}");
+            //foreach (string country in countries)
+            //{
+            //    Console.WriteLine(country);
+            //}
+
+            //Console.WriteLine("=====================RemoveByIndex====================");
+            //countries.RemoveAt(2);
+            //Console.WriteLine($"List count: {countries.Count}");
+            //foreach (string country in countries)
+            //{
+            //    Console.WriteLine(country);
+            //}
+
+            //Console.WriteLine("=====================Contains====================");
+            //bool result = countries.Contains("Ukraine");
+            //Console.WriteLine(result);
+            //Console.WriteLine($"List count: {countries.Count}");
+            //foreach (string country in countries)
+            //{
+            //    Console.WriteLine(country);
+            //}
+
+
+            //========================Benchmark====================
+            var result = BenchmarkRunner.Run<Bench>();
+        }
+    }
+}
---
+++ Benchmark/Bench.cs
@@ -0,0 +1,45 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace Benchmark
+{
+    [MemoryDiagnoser]
+    public class Bench
+    {
+        [Params(1000, 10000, 100000)]
+        public int capacity;
+
+        [Benchmark]
+        public List<int> Add()
+        {
+            List<int> list = new List<int>();
+            for(int i = 0; i < capacity; i++)
+            {
+                list.Add(i);
+            }
+            return list;
+        }
+
+        [Benchmark]
+        public List<int> Incert()
+        {
+            List<int> list = new List<int>();
+            for (int i = 0; i < capacity; i++)
+            {
+                list.Insert(0, i);
+            }
+            return list;
+        }
+
+        [Benchmark]
+        public List<int> AddRange()
+        {
+            int[] arr =  { 1, 3, 5, 7, 9 };
+            List<int> list = new List<int>();
+            for (int i = 0; i < capacity; i++)
+            {
+                list.AddRange(arr);
+            }
+            return list;
+        }
+    }
+}
