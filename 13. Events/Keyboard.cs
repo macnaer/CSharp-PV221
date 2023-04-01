928148e3891cc6b7c62816f2e7178a72cabf02d6
macnaer <trofimchuk.an@gmail.com>
2023-04-01 18:36:11 +0300

    Events

---
+++ 13. Events/Keyboard.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13._Events
+{
+    public delegate void PressKeyEventHandler();
+    internal class Keyboard
+    {
+        public event PressKeyEventHandler PressKeyA = null;
+        public event PressKeyEventHandler PressKeyE = null;
+        public void PressKeyAEvent()
+        {
+            if(PressKeyA != null)
+            {
+                PressKeyA.Invoke();
+            }
+        }
+
+        public void PressKeyEEvent()
+        {
+            if(PressKeyE != null)
+            {
+                PressKeyE.Invoke();
+            }
+        }
+
+        public void Start()
+        {
+            Console.CursorVisible = false;
+            while(true)
+            {
+                string s = Console.ReadLine();
+                Console.Clear();
+                switch(s)
+                {
+                    case "a":
+                    case "A":
+                        PressKeyAEvent(); break;
+                    case "e":
+                    case "E":
+                        PressKeyEEvent(); break;
+                    case "exit":
+                        return;
+                    default:
+                        Console.WriteLine($"Event handler not selected! {s}");
+                        break;
+                }
+            }
+            Console.WriteLine("Exit!");
+        }
+    }
+}
---
+++ 13. Events/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace _13._Events
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Keyboard keyboard = new Keyboard();
+            keyboard.PressKeyA += new PressKeyEventHandler(PressKeyA_Handler);
+            keyboard.PressKeyE += PressKeyE_Handler;
+
+            keyboard.Start();
+        }
+
+        static private void PressKeyA_Handler()
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine();
+            Console.WriteLine("   *    ");
+            Console.WriteLine("  * *   ");
+            Console.WriteLine(" * * *  ");
+            Console.WriteLine("*     * ");
+
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
+
+        static private void PressKeyE_Handler()
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine();
+            Console.WriteLine("****");
+            Console.WriteLine("*   ");
+            Console.WriteLine("****");
+            Console.WriteLine("*   ");
+            Console.WriteLine("****");
+
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
+    }
+}
